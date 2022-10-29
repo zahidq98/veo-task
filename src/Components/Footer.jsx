5dcee4e86c4d1e01e00cec2096067de281278309
Zahid Qadus <zahidqadus98@gmail.com>
2022-10-29 18:05:44 +0100

    Added styling to header and footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,18 @@ import "./Footer.css"
 const Footer = () => {
     return (
       <div className="footer-container">
-          <h1>This is the Footer</h1>
+          <div className="links">
+          <a href="">About Us</a>
+          <a href="">Contact</a>
+          <a href="">Sustainability</a>
+          <a href="">Join as a Brand</a>
+          </div>
+          <div className="more-links">
+          <a href="">Blog</a>
+          <a href="">Delivery</a>
+          <a href="">Returns</a>
+          <a href="">FAQs</a>
+          </div>
       </div>
   
     )
