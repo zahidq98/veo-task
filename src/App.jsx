82059ab34f7aba981d14e5981119df2251f26b4e
Zahid Qadus <zahidqadus98@gmail.com>
2022-10-29 12:06:12 +0100

    Added 2 components - header & footer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+function App() {
+  const name = 'Zahid'
+  return (
+    <div className="Contanier">
+      <Header />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
