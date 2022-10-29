e98381ed5e0e8d7ece1b75e0d7a1045f5e944162
Zahid Qadus <zahidqadus98@gmail.com>
2022-10-29 12:48:40 +0100

    Added products component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Products from "./Components/Products";
 
 function App() {
   const name = 'Zahid'
   return (
     <div className="Contanier">
       <Header />
+      <Products/>
+
+
+
+      
       <Footer/>
+      
     </div>
   );
 }
---
+++ src/Components/Products.jsx
@@ -0,0 +1,13 @@
+
+
+const Products = () => {
+
+    return (
+      <div>
+          <h1>hesdfsd</h1>
+      </div>
+  
+    )
+  }
+  
+  export default Products
