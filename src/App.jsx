7b622789391c56d26f17aa18b16018a775c372e1
Zahid Qadus <zahidqadus98@gmail.com>
2022-10-29 18:56:38 +0100

    Refined the css of all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import ProductsList from "./Components/ProductsList";
+import "./App.css"
 
 function App() {
   return (
--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -5,18 +5,15 @@ import data from "../data/products-example.json"
 
 const ProductsList = () => {
     return (
-        <div className="product-container">
-          <div className="product-list">
-            {data.products.map( product => 
-            <Product
-              title={product.title}
-              image={product.image}
-              description={product.description}
-              />
-              )}
-            </div>
-          </div>
-  
+      <div className="product-container">
+        {data.products.map( product => 
+          <Product
+            title={product.title}
+            image={product.image}
+            description={product.description}
+          />
+        )}
+      </div>
     )
   }
   
