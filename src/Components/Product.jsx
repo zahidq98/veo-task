39f2615541205e0e2a21bf8a2d359ab3a29f3278
Zahid Qadus <zahidqadus98@gmail.com>
2022-10-29 13:23:32 +0100

    Added props to products component

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,17 +1,11 @@
 import "./Product.css"
 
-const Product = () => {
-  const dummyProduct =   {
-      "slug": "hela-cactus-eco-trainer-by-genesis-black",
-      "title": "Hela | Cactus Leather Eco Trainer by Genesis | Black",
-      "image": "https://veo-cdn.imgix.net/products/26687/s_files_1_0863_4804_products_shoes-hela-cactus-eco-trainer-by-genesis-black-1.png?fit=clip&auto=format&w=1080",
-      "description": "• Mesh/mesh fabric made from recycled PET plastic bottles • Cactus based vegan leather • Recycled EVA foam insole • Cork sole from sustainable production • Laces made from recycled PET plastic bottles • Seams made from recycled PET plastic bottles • Lining in Abacá banana canvas and recycled cotton • Sole made from at least 40% natural rubber (unbleached) from Vietnam"
-    }
+const Product = (props) => {
     return (
     <div className="product"> 
-        <img className="product-img" src={dummyProduct.image}/>
-        <h3>{dummyProduct.title}</h3>
-        <p>{dummyProduct.description}</p>
+        <img className="product-img" src={props.image}/>
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
     </div>
   )
 }
--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -2,11 +2,21 @@ import Product from "./Product"
 
 
 const ProductsList = () => {
-
+  const dummyProduct =   {
+      "slug": "hela-cactus-eco-trainer-by-genesis-black",
+      "title": "Hela | Cactus Leather Eco Trainer by Genesis | Black",
+      "image": "https://veo-cdn.imgix.net/products/26687/s_files_1_0863_4804_products_shoes-hela-cactus-eco-trainer-by-genesis-black-1.png?fit=clip&auto=format&w=1080",
+      "description": "• Mesh/mesh fabric made from recycled PET plastic bottles • Cactus based vegan leather • Recycled EVA foam insole • Cork sole from sustainable production • Laces made from recycled PET plastic bottles • Seams made from recycled PET plastic bottles • Lining in Abacá banana canvas and recycled cotton • Sole made from at least 40% natural rubber (unbleached) from Vietnam"
+    }
     return (
       <div>
           <h1>All products</h1>
-          <Product/>
+          <Product 
+            title={dummyProduct.title}
+            image={dummyProduct.image}
+            description={dummyProduct.description}
+            />
+            
       </div>
   
     )
