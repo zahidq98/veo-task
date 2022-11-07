e261b6b91a912d6f956d6693f98ab3761bb7b64c
Zahid Qadus <zahidqadus98@gmail.com>
2022-11-07 19:47:40 +0000

    Added MainContent component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import ProductsList from "./Components/ProductsList";
 import "./App.css"
+import MainContent from "./Components/MainContent";
 
 function App() {
   return (
     <div className="Contanier">
       <Header />
+      <MainContent/>
       <ProductsList/>
-      <Footer/>    
+      <Footer/>   
     </div>
   );
 }
